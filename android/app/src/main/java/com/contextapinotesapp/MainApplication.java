1a2561bc8240545e56f0b183136f34a9c504e1b1
Itamar Axelrad <itamara@aman.co.il>
2022-04-26 21:44:13 +0300

    Added react-native-vector icons library

--- android/app/src/main/java/com/contextapinotesapp/MainApplication.java
+++ android/app/src/main/java/com/contextapinotesapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import com.contextapinotesapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import android.os.Bundle;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -56,6 +58,11 @@ public class MainApplication extends Application implements ReactApplication {
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
