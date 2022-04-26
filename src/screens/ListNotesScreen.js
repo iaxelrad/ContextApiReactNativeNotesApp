d577a30bc90c1984fdba5879ff41f7af18a6e81d
Itamar Axelrad <itamara@aman.co.il>
2022-04-26 22:20:21 +0300

    Added Delete button for every item

--- src/screens/ListNotesScreen.js
+++ src/screens/ListNotesScreen.js
@@ -1,18 +1,32 @@
 import React, {useContext} from 'react';
-import {Text, View, FlatList, Button} from 'react-native';
+import {Text, View, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import {NotesContext} from '../context/NotesContext';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const ListNotesScreen = () => {
   const {state, dispatch} = useContext(NotesContext);
   return (
-    <View>
-      <Button title="add" onPress={() => dispatch({type: 'ADD'})} />
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => dispatch({type: 'ADD'})}>
+          <Icon name="plus" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
       <Text>Notes App</Text>
       <FlatList
         data={state}
         keyExtractor={item => item.title}
         renderItem={({item}) => {
-          return <Text style={{fontSize: 22}}>{item.title}</Text>;
+          return (
+            <View style={styles.item}>
+              <Text style={styles.text}>{item.title}</Text>
+              <TouchableOpacity style={styles.deleteButton} onPress={() => {}}>
+                <Icon name="delete" size={24} />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
@@ -20,3 +34,33 @@ const ListNotesScreen = () => {
 };
 
 export default ListNotesScreen;
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  buttonContainer: {justifyContent: 'center', alignItems: 'center'},
+  button: {
+    backgroundColor: 'blue',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  deleteButton: {
+    // backgroundColor: 'blue',
+    // width: 50,
+    // height: 50,
+    // borderRadius: 50 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    alignItems: 'center',
+  },
+  text: {fontSize: 22},
+});
