142ae77b5dd1a48d74d149515702d254163d3c9b
Itamar Axelrad <itamara@aman.co.il>
2022-04-29 08:50:42 +0300

    Displaying note title and content from NoteContext

--- src/screens/ShowNoteScreen.js
+++ src/screens/ShowNoteScreen.js
@@ -1,14 +1,24 @@
 import React, {useContext} from 'react';
 import {Text, View, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import {NotesContext} from '../context/NotesContext';
 
 const ShowNoteScreen = ({route}) => {
   const {id} = route.params;
+  const {state} = useContext(NotesContext);
+
+  const note = state.find(n => n.id === id);
   return (
-    <View>
-      <Text>Show Note</Text>
-      <Text>{id}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{note.title}</Text>
+      <Text style={styles.content}>{note.content}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1, marginTop: 10},
+  title: {fontSize: 22, textAlign: 'center'},
+  content: {fontSize: 18, textAlign: 'center'},
+});
 
 export default ShowNoteScreen;
